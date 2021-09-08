bf65ab2957123bd94c2e1593a083e504f2938d0c
gocho <gocho@LAPTOP-S5D997K4>
2021-09-08 20:26:03 +0900

    add

--- src/main/java/com/service/seleniumtest.java
+++ src/main/java/com/service/seleniumtest.java
@@ -49,7 +49,7 @@ public class seleniumtest {
      
 	public seleniumtest() {
 	    	String rootPath = System.getProperty("user.dir");
-	      	WEB_DRIVER_PATH = "/usr/local/share/chromedriver"; //rootPath + "/src/main/resources/static/chromedriver"; //  // 
+	      	WEB_DRIVER_PATH = rootPath + "/src/main/resources/static/chromedriver"; //  // 
 	      	download_path = rootPath + "/src/main/resources/static/stock";
 	      	File path = new File(download_path);
 	      	download_path = path.getAbsolutePath();
@@ -97,7 +97,7 @@ public class seleniumtest {
 	         options.addArguments("headless");
 	         options.addArguments("no-sandbox");
 	         options.addArguments("disable-dev-shm-usage");
-	         //options.addArguments("disable-gpu");
+	         options.addArguments("disable-gpu");
 	         options.addArguments("lang=ko_KR");
 	         
 	         
@@ -116,7 +116,7 @@ public class seleniumtest {
 	         
 	         
 	         driver = new ChromeDriver(options);
-	     //    driver.close();
+	         driver.close();
 	         
 	         /*
 	        try {
